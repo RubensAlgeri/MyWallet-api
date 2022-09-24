cec03b2f46650702a6ccd1ac07d338f8a796c93b
Rubens Algeri <rubens_zys@hotmail.com>
2022-09-24 14:30:14 -0300

    fix: removed chalk

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import cors from "cors"
 import router from './routes/index.js';
 
 const app = express();
-app.use(cors())
 app.use(express.json());
+app.use(cors())
 
 app.use(router);
 const porta = process.env.PORT
--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 import { MongoClient } from 'mongodb';
-import chalk from 'chalk';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -8,5 +7,4 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 await mongoClient.connect();
 
 const db = mongoClient.db(process.env.BANCO);
-console.log(chalk.bold.blue("Banco de dados MongoDB conectado!"));
 export default db;
